95a89ef0700645bc55c6c2b1a37af47ddad92927
Carla Apolinário <carla.apolinario.459@ufrn.edu.br>
2023-05-05 21:03:40 -0400

    first commit

---
+++ R/func_secundarias.R
@@ -0,0 +1,58 @@
+#############################
+#funcoes utilizadas no pacote
+#############################
+
+
+#malha de pontos simulados
+Gerarmalha <- function(nn, a = 0, b = 1) {
+  x <- array(NA, dim = c(nn, nn))
+  y <- array(NA, dim = c(nn, nn))
+  malha <- seq(a, b, length = nn)
+  
+  for (i in 1:nn)
+  {
+    for (j in 1:nn)
+    {
+      x[j, i] <- malha[i]
+      y[j, i] <- malha[j]
+    }
+  }
+  
+  xg <- as.matrix(as.vector(x))
+  
+  yg <- as.matrix(as.vector(y))
+  
+  
+  res <- cbind(xg, yg)
+  res
+}
+
+##################################
+#função cria a matriz de covariancia
+gSigma <- function(b, v, def) {
+  n <- nrow(def)
+  R <- exp(-b * (as.matrix(dist(def))))
+  mat <- v * R
+  mat
+}
+
+
+#######################
+#gerando dados da normal multivariada
+
+normalmulti<- function(x, PSI, b, v){
+  
+  #valores necessarios para a normal multivariada
+  #vetor de médias com as covariaveis
+  media<- x%*%PSI
+  
+  #matriz de covariancia
+  SS<-gSigma(b,v,pontos_simulados)
+  
+  #gerando dados de uma normal multivariada
+  dados<- mvrnorm(1, media, SS)
+  
+  return(dados)
+  
+}
+
---
+++ R/prchawkes.R
@@ -0,0 +1,116 @@
+#'Simulador do processo de Hawkes
+#'
+#' Esta é a função para simuar os dados a partir de um processo de Hawkes
+#'
+#' @param n um número inteiro positivo, número de pontos na malha
+#' @param PSI_lambda vetor de covariaveis px1
+#' @param b_lambda número inteiro positivo
+#' @param v_lambda número inteiro positivo, variância do parâmetro lambda
+#' @param PSI_alpha vetor de covariaveis px1
+#' @param b_alpha número inteiro positivo
+#' @param v_alpha número inteiro positivo, variância do parâmetro alpha
+#' @param PSI_beta vetor de covariaveis px1
+#' @param b_beta número inteiro positivo
+#' @param v_beta número inteiro positivo, variância do parâmetro beta
+#' @param horizon número positivo, horizonte dos dados.
+#'
+#' @examples
+#' n<-3
+#' PSI_lambda<-as.matrix(c(1,3,4))
+#' b_lambda<-1
+#' v_lambda<-2
+#' PSI_alpha<-as.matrix(c(1,3,4))
+#'b_alpha<-1
+#'v_alpha<-2
+#'PSI_beta<-as.matrix(c(4,5,6))
+#'b_beta<-1
+#'v_beta<-2
+#'horizon<-360
+#' prchawkes(n, PSI_lambda, b_lambda, v_lambda,PSI_alpha,b_alpha, v_alpha,PSI_beta,b_beta,v_beta,horizon)
+
+
+source("func_secundarias.r")
+
+
+setwd()
+
+#' @export
+prchawkes<-function(n, PSI_lambda, b_lambda, v_lambda,
+                    PSI_alpha,b_alpha, v_alpha,
+                    PSI_beta,b_beta,v_beta,horizon){
+
+  nn<-n^2
+
+  #gerar a malha de pontos simulados (que serão os dados depois - x)
+  pontos_simulados<-Gerarmalha(n)
+
+  #incluindo intercepto
+  x<-cbind(rep(1,nn),pontos_simulados)
+
+
+  ##############lambda###################
+
+  #Vetor W
+  W<-normalmulti(x,PSI_lambda,b_lambda,v_lambda)
+
+  #vetor lambda
+  lambda<- exp(W)
+
+  ##############alpha#################
+
+  #Vetor M
+  M<-normalmulti(x,PSI_alpha,b_alpha,v_alpha)
+
+  #vetor alpha
+  alpha<- exp(M)
+
+
+  ###############beta##################
+
+  #Vetor Q
+  Q<-normalmulti(x,PSI_beta,b_beta,v_beta)
+
+  #vetor beta
+  beta<- exp(Q)
+
+
+
+
+  ################################
+
+  #simulando processo de hawkes
+  #vetor vazio para n^2 listas
+  lista <- vector("list", length = nn)
+
+  #processo de hawkes
+  for (i in 1:nn) {
+    lista[[i]]<-simulateHawkes(lambda[i],alpha[i],beta[i],horizon)
+  }
+
+  #encontrando o maior tamanho
+  maior<-0
+  for (i in 1:nn) {
+    if (length(unlist(lista[[i]])) > maior)
+    {
+      maior<- length(unlist(lista[[i]]))
+    }
+  }
+
+
+  #matriz com todos os dados simulados (cada linha é um processo de Hawkes)
+  #matriz vazia com maior tamanho sendo o processo com mais dados
+  proc_hawkes<- matrix(NA,nn, ncol=maior)
+
+  #alocando cada processo em uma linha diferente
+  for (i in 1:nn) {
+    x<-unlist(lista[[i]])
+    tamanho<- maior-length(x)
+    proc_hawkes[i,]<-c(x,rep(NA,tamanho))
+  }
+
+  #retornando valores
+  res <- list(proc_hawkes, W, M, Q, x)
+  return(res)
+}
+
+
